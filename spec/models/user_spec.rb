9b190b05a9f379955d49d231c19f31905816b0de
nishide <nishide.h@gmail.com>
2017-03-10 15:11:17 +0900

    Update テストコード修正、Usersテーブルの必須項目テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "ユーザの項目 メールアドレス、パスワード、送付先情報（名前、住所）" do
+    is_expected.to respond_to(:email)
+    is_expected.to respond_to(:encrypted_password)
+    is_expected.to respond_to(:to_name)
+    is_expected.to respond_to(:to_address)
+  end
+  
+  before { @user = User.new(
+    #name: "Example User", 
+    email: "user@example.com",
+    #encrypted_password: Devise::Encryptor.digest(User, 'password')
+  ) }
+  subject { @user }
+  
+  #it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  #it { should be_valid }
+
+  #describe "when email is not present" do
+  #  before { @user.email = "" }
+  #  it { should_not be_valid }
+  #end
 end
