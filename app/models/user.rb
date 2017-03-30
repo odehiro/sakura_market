1943e0926366d7ff91671ddf749f3c7fff42070d
nishide <nishide.h@gmail.com>
2017-03-30 15:45:06 +0900

    update deviseに不要な処理だったので削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ApplicationRecord
-  #attr_reader :email
-  attr_accessor :email
-
-  def initialise(attributes = {})
-    @email = attributes[:email]
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
