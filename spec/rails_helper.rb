11af9dc2b8bf835d54031dff00a62bc011f8ddd6
nishide <nishide.h@gmail.com>
2017-03-30 20:53:03 +0900

    add deviseテスト時のプロキシエラー対応

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,6 +53,10 @@ RSpec.configure do |config|
   #
   config.include Capybara::DSL
 
+  # Devise Gemによるプロキシエラー対応
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
