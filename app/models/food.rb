9193a07796445be1cd9d6204257959c6109c9f39
nishide <nishide.h@gmail.com>
2017-04-07 10:50:46 +0900

    add Food Model実装

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :caption, presence: true
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -10,4 +10,36 @@ RSpec.describe Food, type: :model do
     is_expected.to respond_to(:display)
     is_expected.to respond_to(:order)
   end
+
+  before do
+    #path = Rails.root.join("画像が置いてあるパス")
+    #testImage = File.open(path, "rb").read
+    @food = Food.new(
+      name: "玄米",
+      image: "",
+      price: 1000,
+      caption: "発芽すると栄養豊富",
+      display: true,
+      order: ""
+    )
+  end
+
+  subject { @food }
+
+  it { should be_valid }
+
+  describe "名称が空白でないこと" do
+    before {@food.name = ""}
+    it { should_not be_valid }
+  end
+
+  describe "金額が空白でないこと" do
+    before {@food.price = ""}
+    it { should_not be_valid }
+  end
+
+  describe "商品説明が空白でないこと" do
+    before {@food.caption = ""}
+    it { should_not be_valid }
+  end
 end
