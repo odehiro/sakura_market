164f873e0ec6d1ab6b85bd7f989b4bf3785cc69c
nishide <nishide.h@gmail.com>
2017-05-24 14:31:46 +0900

    add "LineItems Table"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524025743) do
+ActiveRecord::Schema.define(version: 20170524052940) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170524025743) do
     t.text     "caption"
     t.boolean  "display"
     t.string   "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "food_id"
+    t.integer  "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
