5993d7cffc1ec8b97bb1dae759c24e047b3904ba
nishide <nishide.h@gmail.com>
2017-05-25 14:36:11 +0900

    update "cartにリレーション追加"

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "cart.create" do
+    context "正常系" do
+      it "create valid" do
+        cart = Cart.create
+        expect(cart).to be_valid
+      end
+    end
+  end
 end
