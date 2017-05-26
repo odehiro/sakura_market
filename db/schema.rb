1de1605fad00361f6fbe0122633732c6711f8348
nishide <nishide.h@gmail.com>
2017-05-26 15:06:33 +0900

    add "カートの品目に数量カラム"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524052940) do
+ActiveRecord::Schema.define(version: 20170526044101) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20170524052940) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "food_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
   end
 
   create_table "users", force: :cascade do |t|
