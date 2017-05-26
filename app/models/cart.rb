54d82a8751172454d68f727db00ff0b2ca33ea98
nishide <nishide.h@gmail.com>
2017-05-26 15:08:35 +0900

    add "Cart Modelに品目追加処理"

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  
+  def add_food(food_id)
+    current_item = line_items.find_by_food_id(food_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(food_id: food_id)
+    end
+
+    current_item
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,4 +9,35 @@ RSpec.describe Cart, type: :model do
       end
     end
   end
+
+  describe "cart.add_food" do
+    let(:cart){ FactoryGirl.create(:cart_valid) }
+    let(:food) { FactoryGirl.create(:food) }
+    let(:food2) { FactoryGirl.create(:food2) }
+
+    before do
+      cart.line_items.create(food: food)
+    end
+
+    context "同じ品目の場合" do
+      it "対象品目の個数が増えること" do
+        line_item = cart.line_items.find_by_food_id(food.id)
+        expect(line_item.quantity).to eq 1
+
+        line_item = cart.add_food(food.id) 
+        expect(line_item.quantity).to eq 2
+      end
+    end
+
+    context "違う商品の場合" do
+      it "品目が増えること" do
+        line_item = cart.line_items.find_by_food_id(food.id)
+        expect(line_item.quantity).to eq 1
+
+        line_item = cart.add_food(food2.id) 
+        expect(line_item.food_id).to eq food2.id
+        expect(line_item.quantity).to eq 1
+      end
+    end
+  end
 end
