701767748342244ae58c2473404f66be1e7e35d5
nishide <nishide.h@gmail.com>
2017-06-02 10:58:47 +0900

    update "Index(Welcome画面)のテストコード"

--- spec/requests/index_pages_spec.rb
+++ spec/requests/index_pages_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "IndexPages", type: :request do
-  describe "GET /index_pages" do
-    before { visit root_path }
-    subject { page }
+RSpec.feature "IndexPages", type: :request do
+  subject { page }
+  
+  feature "GET /index_pages" do
+    background { visit root_path }
 
-    describe "項目確認" do
-      it { should have_http_status(200) }
-      it { should have_link("サインイン") }
+    feature "項目確認" do
+      scenario { is_expected.to have_http_status(200) }
+      scenario { is_expected.to have_link("サインイン") }
     end
 
-    describe "ヘッダー項目" do
-      it { should have_link("さくらマーケット", href: root_path) }
-      it { should_not have_link("日記を見る") }
-      it { should_not have_link("お買い物") }
-      it { should_not have_link("ショッピングカート") }
-      it { should have_link("ログイン", href: new_user_session_path) }
+    feature "ヘッダー項目" do
+      scenario { is_expected.to have_link("さくらマーケット", href: root_path) }
+      scenario { is_expected.not_to have_link("日記を見る") }
+      scenario { is_expected.not_to have_link("お買い物") }
+      scenario { is_expected.not_to have_link("ショッピングカート") }
+      scenario { is_expected.to have_link("ログイン", href: new_user_session_path) }
     end
 
-    it "サインインボタンクリック" do
-      click_on "サインイン"
-      expect(current_path).to eq new_user_registration_path
+    feature "devise#registrations/new" do
+      scenario "サインイン画面へ遷移できること" do
+        click_on "サインイン"
+        expect(current_path).to eq new_user_registration_path
+      end
     end
-  end
 
-  describe "signup" do
-  #  before { visit new_user_registration_path }
-  #  let(:submit) { "" }
-
-  #  if "should not create a user" do
-  #    expect { click_button submit }.not_to change(User, :count)
-  #  end
+    feature "devise#sessions/new" do
+      scenario "ログイン画面へ遷移できること" do
+        click_on "ログイン"
+        expect(current_path).to eq new_user_session_path
+      end
+    end
   end
 end
