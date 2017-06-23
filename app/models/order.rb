9519c9afc0c277266509582eaf4e9bcd8e660663
nishide <nishide.h@gmail.com>
2017-06-23 13:54:38 +0900

    add "注文モデル、テストコード"

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  PAYMENT_TYPES = ["代引き"]
+
+  validates :name, :address, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ before do
+    @order = Order.new(
+      name: "User",
+      address: "京都府京都市",
+      pay_type: "代引き"
+    )
+  end
+
+  subject { @order }
+
+  describe ".new" do
+    it "beforeのデータが作成可能であること" do
+      is_expected.to be_valid
+    end
+
+    it "名前が空白は有効でないこと" do
+      @order.name = ""
+      is_expected.not_to be_valid
+    end
+
+    it "住所が空白は有効でないこと" do
+      @order.address = ""
+      is_expected.not_to be_valid
+    end
+    
+    it "支払い方法が選択されていること" do
+      @order.pay_type = "カード"
+      is_expected.not_to be_valid
+    end
+  end
 end
