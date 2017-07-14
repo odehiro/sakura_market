1e7a8605aad7d8db7545b550128aef95556b6615
nishide <nishide.h@gmail.com>
2017-07-14 16:03:07 +0900

    update "合計行を税込みに"

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   
@@ -10,9 +12,10 @@ class Cart < ApplicationRecord
     end
 
     current_item
-  end
+  end 
 
   def total_price
-    line_items.to_a.sum { |item| item.total_price }
+    price =line_items.to_a.sum { |item| item.total_price }
+    (BigDecimal(price.to_s) * BigDecimal(Settings.price.consumption_tax)).floor
   end
 end
