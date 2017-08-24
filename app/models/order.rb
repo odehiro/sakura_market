4f564d9e6b81d09a760ef63b8464b3084321875c
nishide <nishide.h@gmail.com>
2017-08-24 16:01:32 +0900

    add "小計、代引き金額、合計額表示"

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,21 @@ class Order < ApplicationRecord
 
   validates :name, :address, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
+  
+  @total = 0
+  @cashOnDeliveryPrice = 0
+
+  def total_price
+    @total = line_items.to_a.sum { |item| item.total_price }
+  end
+  
+  def cash_on_delivery_price
+    @cashOnDeliveryPrice = CashOnDelivery.where("less > #{ @total }").first.price
+  end
+  
+  def all_total_price
+    @total + @cashOnDeliveryPrice
+  end
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
