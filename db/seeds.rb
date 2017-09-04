582f6f3818496596e275188a4946d7482ee1ec70
nishide <nishide.h@gmail.com>
2017-09-04 09:44:18 +0900

    update "orderが文字列だったので修正"

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,40 +25,40 @@ Food.create(name: "さくらんぼ",
             price: 100,
             caption: "甘くて美味しい",
             display: true,
-            order: ""
+            order: 1
            )
 Food.create(name: "キャベツ",
             image: File.open("./db/images/kyabetsu_cabbage.png").read,
             price: 2000,
             caption: "シャッキシャキ",
             display: true,
-            order: ""
+            order: 2
            )
 Food.create(name: "だいこん",
             image: File.open("./db/images/daikon_radish.png").read,
             price: 150,
             caption: "聖護院の京都産",
             display: true,
-            order: ""
+            order: 3
            )
 Food.create(name: "白菜",
             image: File.open("./db/images/hakusai.png").read,
             price: 290,
             caption: "お鍋にぴったり",
             display: true,
-            order: ""
+            order: 4
            )
 Food.create(name: "にら",
             image: File.open("./db/images/nira15.jpg").read,
             price: 100,
             caption: "レバーとどうぞ",
             display: true,
-            order: ""
+            order: 5
            )
 Food.create(name: "メロン",
             image: File.open("./db/images/fruit_melon_cut.png").read,
             price: 2000,
             caption: "夕張産です",
             display: true,
-            order: ""
+            order: 6
            )
