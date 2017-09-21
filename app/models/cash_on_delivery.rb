9367f02ba6901255597ce0ac97016e1e08f7351f
nishide <nishide.h@gmail.com>
2017-09-21 13:59:07 +0900

    add "cash_on_deliveryモデルに入力必須、そのテストコード"

--- app/models/cash_on_delivery.rb
+++ app/models/cash_on_delivery.rb
@@ -1,2 +1,4 @@
 class CashOnDelivery < ApplicationRecord
+  validates :less, presence: true
+  validates :price, presence: true
 end
--- spec/models/cash_on_delivery_spec.rb
+++ spec/models/cash_on_delivery_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CashOnDelivery, type: :model do
-  xdescribe ".create" do
-    context "正常系" do
-      it "create valid" do
-        cash = CashOnDelivery.create
-        expect(cash).to be_valid
+  describe ".create" do
+    before do
+      @cash = CashOnDelivery.new(
+        less: 500,
+        price: 1000
+      )
+    end
+
+    describe ".new" do
+      it "beforeのデータが作成できること" do
+        expect(@cash).to be_valid
+      end
+
+      it "less(未満の値)が空白は有効でないこと" do
+        @cash.less = nil
+        expect(@cash).not_to be_valid
+      end
+
+      it "金額が空白は有効でないこと" do
+        @cash.price = nil
+        expect(@cash).not_to be_valid
       end
     end
   end
