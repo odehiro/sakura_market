994257765c29ecea767e4a726de6cbc8212a39b6
nishide <nishide.h@gmail.com>
2017-10-06 15:59:40 +0900

    fix "orderという名称がダメかも。シンボルにしてみた。"

--- db/migrate/20170530024231_change_datatype_order_of_foods.rb
+++ db/migrate/20170530024231_change_datatype_order_of_foods.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeOrderOfFoods < ActiveRecord::Migration[5.0]
   def change
-    change_column :foods, :order, 'integer USING CAST(order AS integer)'
+    change_column :foods, :order, 'integer USING CAST(:order AS integer)'
   end
 end
